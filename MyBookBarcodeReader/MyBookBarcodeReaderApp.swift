5891b2ebc4081f7933c0124dc61e6a2f47b185ae
wolfg1969 <wolfg1969@gmail.com>
2023-09-09 16:06:06 +0800

    Initial Commit

---
+++ MyBookBarcodeReader/MyBookBarcodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookBarcodeReaderApp.swift
+//  MyBookBarcodeReader
+//
+//  Created by 郭勇 on 2023/9/9.
+//
+
+import SwiftUI
+
+@main
+struct MyBookBarcodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
